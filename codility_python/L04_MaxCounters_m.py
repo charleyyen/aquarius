b389a1091dff4b773f9498124620636149cceada
charleyyen <charley.yen@gmail.com>
2023-02-11 23:12:53 -0500

    This version contains multiple different implementations with different scores. A good example to help understanding how to enhance the performance.

--- codility_python/L04_MaxCounters_m.py
+++ codility_python/L04_MaxCounters_m.py
@@ -65,19 +65,125 @@ Copyright 2009–2022 by Codility Limited. All Rights Reserved.
 Unauthorized copying, publication or disclosure prohibited.
 
 """
-def solution(N, A):
+def solution_1_77_100_60(N, A):
     # Task Score 77% Correctness 100% Performance 60%
-    arr = [0]*N
-    max = 0
+    arr = [0]*(N+1)
+    maximum = 0
     for e in A:
         if e > N:
-            arr = [max]*N
+            arr = [maximum]*(N+1) # time consuming
+        else:
+            arr[e] += 1
+            if arr[e] > maximum:
+                maximum = arr[e]
+
+    return arr[1:]
+
+def solution_2_66_100_40(N, A):
+    """
+    Detected time complexity: O(N*M)
+    Task Score 66% Correctness 100% Performance 40%
+    large_random1: large random test, 2120 max_counter operations
+    TIMEOUT ERROR
+    running time: 1.040 sec., time limit: 0.272 sec.
+    large_random2: large random test, 10000 max_counter operations
+    TIMEOUT ERROR
+    running time: 4.724 sec., time limit: 0.528 sec.
+    extreme_large: all max_counter operations
+    TIMEOUT ERROR
+    Killed. Hard limit reached: 6.000 sec.
+    """
+    arr = [0]*(N)
+    maxium = 0
+    for e in A:
+        if e > N:
+            maxium += sorted(arr)[-1] # Time consuming
+            arr = [0]*(N) # Time consuming
         else:
             arr[e-1] += 1
-            if arr[e-1] > max:
-                max = arr[e-1]
+
+    return [x + maxium for x in arr]
+    #return list(map(lambda x : x + maxium, arr)) # same score
+
+def solution_3_66_100_40(N, A):
+    # Task Score 66% Correctness 100% Performance 40%
+    arr = [0]*(N)
+    maxium = 0
+    for e in A:
+        if e > N:
+            maxium = sorted(arr)[-1] # Time consuming
+            arr = [maxium]*(N) # Time consuming
+        else:
+            arr[e-1] += 1
 
     return arr
 
-data = [3,4,4,6,1,4,4]
+def solution_4_66_100_40(N, A):
+    # Task Score 66% Correctness 100% Performance 40%
+    arr = [0]*(N)
+    for e in A:
+        if e > N:
+            arr = [max(arr)]*(N) # Time consuming
+        else:
+            arr[e-1] += 1
+
+    return arr
+
+
+# ---------------------------------------------------------------
+# I new the implementation below would fail the performance test.
+# But I did not expected it scored ZERO, because it failed the
+# performance test on EVERY sigle case!!
+# ---------------------------------------------------------------
+from collections import Counter
+
+def solution_5_0(N, A):
+    # Only passed the sample data. Score: 0
+    indices = [i for i, x in enumerate(A) if x == N+1]
+    j = 0
+    maximum = 0
+    for i, e in enumerate(indices):
+        if i > 0:
+            j = indices[i-1] + 1
+        block = A[j:e]
+        data = Counter(block)
+        maximum += block.count(max(block, key=data.get))
+    
+    arr = [maximum]*(N)
+    for e in A[indices[-1]+1:]:
+        arr[e-1] += 1
+
+    return arr
+# end of solution_5_0()
+
+def solution(N, A):
+    """
+    This solution is very much like the first solution - solution_1_77_100_60(N, A).
+    The only difference is that when i > N, we save the maximum instead of assigning
+    it to arr (arr = [maximum]). By doing so, it passed the performance test.
+    """
+    arr = [0]*(N)
+    minimum = maximum = 0
+    for i in A:
+        if i > N:
+            minimum = maximum
+        else:
+            arr[i-1] = max(arr[i-1], minimum)
+            arr[i-1] += 1
+            if arr[i-1] > maximum:
+                maximum = arr[i-1]
+
+    for i in range(len(arr)):
+        arr[i] = max(arr[i], minimum)
+
+    return arr
+
+
+data = [1, 3, 6, 4, 1, 2]
+data = [3, 4, 6, 4, 6, 1, 4, 4]
+data = [6,3,4,4,6,1,1,6,2,2,2,6,3,3,3,3,3,3,6,1,4,4,6]
+data = [3, 4, 4, 6, 1, 1, 2, 2, 2, 3, 6, 5, 3, 3, 3, 3, 3, 6, 1, 4, 4]
+print(solution_1_77_100_60(5, data))
+print()
 print(solution(5, data))
+#print(solution(2, [1, 1, 3, 2, 2]))
