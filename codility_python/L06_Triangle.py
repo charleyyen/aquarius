623f4b87a7dd63c54926d501162ea24b974c53f8
charleyyen <charley.yen@gmail.com>
2023-02-19 18:07:00 -0500

    Init checkin

---
+++ codility_python/L06_Triangle.py
@@ -0,0 +1,49 @@
+"""
+An array A consisting of N integers is given. A triplet (P, Q, R) is triangular if 0 ≤ P < Q < R < N and:
+
+        A[P] + A[Q] > A[R],
+        A[Q] + A[R] > A[P],
+        A[R] + A[P] > A[Q].
+
+For example, consider array A such that:
+  A[0] = 10    A[1] = 2    A[2] = 5
+  A[3] = 1     A[4] = 8    A[5] = 20
+
+Triplet (0, 2, 4) is triangular.
+
+Write a function:
+
+    def solution(A)
+
+that, given an array A consisting of N integers, returns 1 if there exists a triangular triplet for this array and returns 0 otherwise.
+
+For example, given array A such that:
+  A[0] = 10    A[1] = 2    A[2] = 5
+  A[3] = 1     A[4] = 8    A[5] = 20
+
+the function should return 1, as explained above. Given array A such that:
+  A[0] = 10    A[1] = 50    A[2] = 5
+  A[3] = 1
+
+the function should return 0.
+
+Write an efficient algorithm for the following assumptions:
+
+        N is an integer within the range [0..100,000];
+        each element of array A is an integer within the range [−2,147,483,648..2,147,483,647].
+
+Copyright 2009–2023 by Codility Limited. All Rights Reserved. Unauthorized copying, publication or disclosure prohibited.
+"""
+def is_triangle(x,y,z):
+    return 1 if x+y>z and x+z>y and z+y>x else 0
+
+def solution(A):
+    A = sorted(A)
+    for i in range(2, len(A)):
+        if is_triangle(A[i-2], A[i-1], A[i]):
+            return 1
+    return 0
+
+if __name__ == '__main__':
+    A=[10,2,5,1,8,20]
+    print(solution(A))
