d59e30daad0996c28c6701ebc97a1717c6876559
charleyyen <charley.yen@gmail.com>
2023-03-01 17:57:57 -0500

    A clean version

--- codility_python/L09_MaxSliceSum.py
+++ codility_python/L09_MaxSliceSum.py
@@ -1,37 +1,61 @@
-bogus = 1.11
+"""
+A non-empty array A consisting of N integers is given. A pair of integers
+(P, Q), such that 0 ≤ P ≤ Q < N, is called a slice of array A. The sum
+of a slice (P, Q) is the total of A[P] + A[P+1] + ... + A[Q].
 
-def handle_special_cases(arr):
+Write a function:
 
-    return bogus
+    def solution(A)
 
+that, given an array A consisting of N integers, returns the maximum sum
+of any slice of A.
 
-def solution(A):
-    # Score 100%
-    if max(A) < 0:
-        # All negative
-        return sorted(A)[-1]
-    if min(A) > 0:
-        # All positive
-        return sum(A)
-    if len(A) == 1:
-        return A[0]
-    if len(A) == 2:
-        return max(A)
+For example, given array A such that:
+A[0] = 3  A[1] = 2  A[2] = -6
+A[3] = 4  A[4] = 0
 
-    first_positive = next(x for x, val in enumerate(A) if val > 0)
+the function should return 5 because:
 
-    A.append(-1)
+        (3, 4) is a slice of A that has sum 4,
+        (2, 2) is a slice of A that has sum −6,
+        (0, 1) is a slice of A that has sum 5,
+        no other slice of A has sum greater than (0, 1).
+
+Write an efficient algorithm for the following assumptions:
+
+        N is an integer within the range [1..1,000,000];
+        each element of array A is an integer within the range [−1,000,000..1,000,000];
+        the result will be an integer within the range [−2,147,483,648..2,147,483,647].
+
+Copyright 2009–2023 by Codility Limited. All Rights Reserved. Unauthorized
+copying, publication or disclosure prohibited.
+"""
+def solution(array_):
+    """Find a maximum sum of a compact subsequence of array elements. Score 100%"""
+    if max(array_) < 0:
+        # array_ll negative
+        return sorted(array_)[-1]
+    if min(array_) > 0:
+        # array_ll positive
+        return sum(array_)
+    if len(array_) == 1:
+        return array_[0]
+    if len(array_) == 2:
+        return max(array_)
+
+    array_.append(-1)
     all_positive = 0
     max_positive = 0
-    for x in A[first_positive:]:
-        if x > -1:
-            all_positive += x
+    for _ in array_:
+        if _ > -1:
+            all_positive += _
         else:
             max_positive = max(max_positive, all_positive)
-            all_positive += x
+            all_positive += _
             if all_positive < 0:
                 all_positive = 0
                 continue
+
     return max_positive
 
 
@@ -39,5 +63,4 @@ if __name__ == '__main__':
     arr = [30,-1,2,-1,9,-6,4,0,-1]
     arr = [3,2,-6,4,0]
     arr = [-1,99,-98,94,5]
-    solution = solution_4
     print(solution(arr))
