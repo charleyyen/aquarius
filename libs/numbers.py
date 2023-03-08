4c0d5cf0ba2768191bb9132a1936f1afe4e7d461
charleyyen <charley.yen@gmail.com>
2023-03-08 10:50:18 -0500

    Added find_all_primes_in_range_3() and find_all_primes_in_range_4(). Plus some other minor tweaks

--- libs/numbers.py
+++ libs/numbers.py
@@ -47,12 +47,13 @@ def is_prime_2(number, itr=None):
         Recursive function Call
         RecursionError: maximum recursion depth exceeded while calling a Python object
     """
+    function_name = inspect.currentframe().f_code.co_name
     up_limit = 994_012
     if number > up_limit:
         # Cannot handle this number
-        print(f'Cannot handle this number: "{number}"')
+        print(f'{function_name}(): Cannot handle this number: "{number}"')
         print(f'The largest number this method can handle: {up_limit}')
-        return None
+        return 'N/A'
 
     if not isinstance(number, int):
         return False
@@ -136,11 +137,59 @@ def find_all_primes_in_range_1(start=1, end=2_147_483_647):
         return None
     return a_set
 
-def find_all_primes_in_range_2(start=1, end=2_147_483_647):
+def find_all_primes_in_range_2(start=1, end=10_000_000): #end=2_147_483_647
+    # When end is too large, say 500_000_000, it has performance issue.
     #import sympy
     #return list(sympy.sieve.primerange(start, end))
     # draft
     return list(sorted(set(range(start,end+1)).difference(set((p * f) for p in range(2, int(end ** 0.5) + 2) for f in range(2, int(end/p) + 1)))))
+
+def find_all_primes_in_range_3(start=1, end=2_147_483_647):
+    # Super Low efficient
+    # https://www.faceprep.in/c/find-prime-numbers-in-a-given-range-in-c-c-java-and-python-faceprep/
+    list_ = []
+    while start < end:
+        flag = 0
+        for i in range(2, int(start/2), 1):
+            if start%i == 0:
+                flag = 1
+                break
+        if flag == 0:
+            list_.append(i)
+        start += 1
+
+    return list_
+
+
+def find_all_primes_in_range_4(start=1, end=2_147_483_647):
+    # Low efficient
+    list_ = []
+    # MAKING SURE THAT a IS ODD BEFORE WE BEGIN
+    # THE LOOP
+    a = start
+    b = end
+    if (a % 2 == 0):
+        a+=1
+    # NOTE : WE TRAVERSE THROUGH ODD NUMBERS ONLY
+    for i in range(a,b+1,2):
+        # flag variable to tell
+        # if i is prime or not
+        flag = 1
+        # WE TRAVERSE TILL SQUARE ROOT OF j only.
+        # (LARGEST POSSIBLE VALUE OF A PRIME FACTOR)
+        j = 2
+        while(j * j <= i):
+            if (i % j == 0):
+                flag = 0
+                break
+            j+=1
+        # flag = 1 means i is prime
+        # and flag = 0 means i is not prime
+        if (flag == 1):
+            list_.append(i)
+
+    return list_
+
 
 # --- test ---
 def test_is_prime(number=0):
@@ -164,7 +213,7 @@ def test_is_prime(number=0):
         start = time.time()
         answer = a_method(n)
         elapsed = round(time.time() - start, 4)
-        #print(method, answer, elapsed)
+        print(method, answer, elapsed)
         summary.append((method, answer, elapsed))
     my_test.display_summary(summary)
     print()
@@ -203,27 +252,30 @@ def test_find_largest_prime(number=0):
 
 
 def test_find_all_primes_in_range():
-    i_0 = 100
-    i_1 = 10_000_000
-    start = time.time()
-    primes_in_set = find_all_primes_in_range_1(start=i_0, end=i_1)
-    elapsed = round(time.time() - start, 4)
-    print(f'1. All primes between {i_0} and {i_1} (Total count: {len(primes_in_set)})')
-    print(f'Time consumed: {elapsed}')
-    start = time.time()
-    primes_in_set = find_all_primes_in_range_2(start=i_0, end=i_1)
-    print(f'2. All primes between {i_0} and {i_1} (Total count: {len(primes_in_set)})')
-    #:\n{sorted(list(primes_in_set))}')
-    print(f'Time consumed: {elapsed}')
-    print()
+    i_0 = 1
+    i_1 = 1_000_000
+    method_list = [
+        #find_all_primes_in_range_1,
+        find_all_primes_in_range_2,
+        #find_all_primes_in_range_3,
+        #find_all_primes_in_range_4,
+    ]
+
+    for j, a_method in enumerate(method_list, start=1):
+        method = str(a_method).split()[1]
+        start = time.time()
+        answer = a_method(start=i_0, end=i_1)
+        elapsed = round(time.time() - start, 4)
+        print(f'{method}, {len(answer)}, {answer[-5:]}, {elapsed}')
 
 if __name__ == '__main__':
     number = 994013 - 1
+    number = 999983
 
     #test_find_prime_factors(number=number)
     #test_find_largest_prime(number=number)
 
-    #test_is_prime(number=number)
+    test_is_prime(number=number)
     """
     n=2_147_483_647
     1, is_prime_1 - True: Time Consumed: 129.8231
@@ -231,4 +283,4 @@ if __name__ == '__main__':
     3, is_prime_4 - True: Time Consumed: 0.0054
     """
 
-    test_find_all_primes_in_range()
+    #test_find_all_primes_in_range()
