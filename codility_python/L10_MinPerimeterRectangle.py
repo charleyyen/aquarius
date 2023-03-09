e2ca6722ec7738948f11b2d82d2cc7f553fbdcdd
charleyyen <charley.yen@gmail.com>
2023-03-08 22:01:36 -0500

    Added another implementation, which is faster

--- codility_python/L10_MinPerimeterRectangle.py
+++ codility_python/L10_MinPerimeterRectangle.py
@@ -31,8 +31,10 @@ Copyright 2009–2023 by Codility Limited. All Rights Reserved. Unauthorized
 copying, publication or disclosure prohibited. 
 """
 
-def solution(area):
-    width = int(area**0.5)
+import math
+
+def solution_1(area):
+    width = int(math.sqrt(area))
     i, j = width, width
     while area%i != 0 and area%j != 0:
         i += 1
@@ -41,7 +43,34 @@ def solution(area):
     width = i if area%i == 0 else j
     return 2*(width + int(area/width))
 
+
+def solution_2(N):
+    # https://codesays.com/2014/solution-to-min-perimeter-rectangle-by-codility/.
+    for i in range(int(math.sqrt(N)), 0, -1):
+        if N % i == 0:
+            return (int(N / i) + i) * 2
+
 if __name__ == '__main__':
-    area = 101
-    answer = solution(area)
-    print(f'answer: {answer}')
+    # N is an integer within the range [1..1,000,000,000].
+    solution_list = [
+            solution_1,
+            solution_2,
+            ]
+    import my_test # in house
+    import random
+    import time
+
+    low = 100_000_000
+    high = low * 10
+    random_number = random.randint(low, high)
+    print(f'random_number: {random_number:,}')
+    summary = []
+    for j, solution in enumerate(solution_list, start=1):
+        method = str(solution).split()[1]
+        start = time.time()
+        answer = solution(random_number)
+        elapsed = round(time.time() - start, 4)
+        #print(method, answer, elapsed)
+        summary.append((method, answer, elapsed))
+    my_test.display_summary(summary)
+
